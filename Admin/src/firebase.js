349218a1b103a95007b26c0f934746496af255f5
shivam1549 <54504024+shivam1549@users.noreply.github.com>
2024-04-28 21:46:48 +0530

    Update firebase.js

--- Admin/src/firebase.js
+++ Admin/src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD_2iE9C2_GB9gOLEl1AWWHUCyccc5QDac",
-  authDomain: "shop-48d27.firebaseapp.com",
-  projectId: "shop-48d27",
-  storageBucket: "shop-48d27.appspot.com",
-  messagingSenderId: "912250787392",
-  appId: "1:912250787392:web:ccd698b3510385704631a5"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
